aecbbc9225a8c52bcc15ec30eda931e95fd4fb3e
Bashar <Bashar@gmail.com>
2025-10-08 02:15:01 +0600

    try to make this

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <h1>
-          Wellcome to Bashar portfolio
-        </h1>
+    <main className="relative">
+      <div className="relative">
+        <h1> Wellcome to Bashar portfolio</h1>
       </div>
     </main>
   );
