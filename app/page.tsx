d1b49597b9479f0f54335b3a04ec259e13522d01
Bashar <Bashar@gmail.com>
2025-10-10 01:38:30 +0600

    little

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative">
+    <main className="relative bg-black">
       <div className="relative">
         <h1> Wellcome to Bashar portfolio</h1>
       </div>
