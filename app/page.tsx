6185b5b0ea591b79fbc8f659e33aa9a558ce8127
Bashar <Bashar@gmail.com>
2025-10-12 01:33:41 +0600

    Shadcn pakage add

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+
+import Hero from "@/component/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black">
-      <div className="relative">
-        <h1> Wellcome to Bashar portfolio</h1>
+    <main className="relative bg-black flex items-center justify-center overflow-hidden 
+    flex-col mx-auto sm:px-10 px-5">
+      <div className="max-w-2xl w-full">
+       <Hero />
       </div>
     </main>
   );
